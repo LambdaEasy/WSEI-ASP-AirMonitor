9e7c6df5bbb0d023235e87cb51dd430ccea3a9ad
cafetamine <arek.bystrowicz@gmail.com>
2020-12-26 12:22:03 +0100

    rename fields to represent value stored

--- AirMonitor.Domain/Installation/InstallationSponsor.cs
+++ AirMonitor.Domain/Installation/InstallationSponsor.cs
@@ -5,7 +5,7 @@ namespace AirMonitor.Domain.Installation
         public long? InstallationId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Logo { get; set; }
-        public string Link { get; set; }
+        public string LogoUri { get; set; }
+        public string LinkUri { get; set; }
     }
 }
