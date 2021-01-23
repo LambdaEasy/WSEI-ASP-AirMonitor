325eab7e069db52f0a0a445eca7b777ff3a1d2a0
cafetamine <arek.bystrowicz@gmail.com>
2021-01-23 12:05:36 +0100

    implement dao

--- AirMonitor.Persistence/Installation/Repository/InstallationDao.cs
+++ AirMonitor.Persistence/Installation/Repository/InstallationDao.cs
@@ -1,47 +1,75 @@
 using System.Collections.Generic;
+using System.Linq;
 using AirMonitor.Persistence.Installation.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirMonitor.Persistence.Installation.Repository
 {
-    public class InstallationDao
+    public class InstallationDao : DbContext
     {
+        #region DbTables
+
+        private DbSet<InstallationEntity> Installations { get; set; }
+        private DbSet<InstallationAddressEntity> InstallationAddresses { get; set; }
+        private DbSet<InstallationSponsorEntity> InstallationSponsors { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public InstallationDao(DbContextOptions options) : base(options) { }
+
+        #endregion
 
         public bool ExistsByExternalId(long? id)
-        {
-            throw new System.NotImplementedException();
-        }
+            => FindByExternalId(id) != null;
 
         public InstallationEntity Save(InstallationEntity installation)
         {
-            throw new System.NotImplementedException();
+            Installations.Add(installation);
+            SaveChanges();
+            return installation; // TODO verify id exists on saved entity
         }
 
-        public InstallationEntity FindById(long id)
-        {
-            throw new System.NotImplementedException();
-        }
+        public InstallationEntity FindById(long? id)
+            => id == null
+             ? null
+             : Installations.Include(installation => installation.Address)
+                            .Include(installation => installation.Sponsor)
+                            .SingleOrDefault(installation => installation.Id == id);
 
-        public InstallationEntity FindByExternalId(long externalId)
-        {
-            throw new System.NotImplementedException();
-        }
+        public InstallationEntity FindByExternalId(long? externalId)
+            => externalId == null
+             ? null
+             : Installations.Include(installation => installation.Address)
+                            .Include(installation => installation.Sponsor)
+                            .SingleOrDefault(installation => installation.ExternalId == externalId);
 
         public HashSet<InstallationEntity> FindAll()
-        {
-            throw new System.NotImplementedException();
-        }
+            => Installations.Include(installation => installation.Address)
+                            .Include(installation => installation.Sponsor)
+                            .ToHashSet();
 
         public HashSet<InstallationEntity> FindAllWhereLocationInAndLongitudeIn(float minLatitude,
                                                                                 float maxLatitude,
                                                                                 float minLongitude,
                                                                                 float maxLongitude)
-        {
-            throw new System.NotImplementedException(); 
-        }
+            => Installations.Include(installation => installation.Address)
+                            .Include(installation => installation.Sponsor)
+                            .Where(installation => installation.Latitude >= minLatitude && installation.Latitude <= maxLatitude)
+                            .Where(installation => installation.Longitude >= minLongitude && installation.Longitude <= maxLongitude)
+                            .ToHashSet();
 
         public bool DeleteById(long id)
         {
-            throw new System.NotImplementedException(); 
+            InstallationEntity installation = FindById(id);
+            if (installation == null)
+            {
+                return false;
+            }
+            Installations.Remove(installation);
+            SaveChanges();
+            return true;
         }
     }
 }
