09d4c364d20550c802409402930c594daf215d31
cafetamine <arek.bystrowicz@gmail.com>
2021-02-12 19:41:40 +0100

    generate migrations for measurements

---
+++ AirMonitor.Persistence/Migrations/20210212142755_AddMeasurementSchema.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirMonitor.Persistence.Migrations
+{
+    public partial class AddMeasurementSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Measurement",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UpdateDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    FromDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    TillDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurement", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasurementIndexes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", maxLength: 256, nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Advice = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    MeasurementId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementIndexes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeasurementIndexes_Measurement_MeasurementId",
+                        column: x => x.MeasurementId,
+                        principalTable: "Measurement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasurementStandards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Pollutant = table.Column<int>(type: "int", nullable: false),
+                    Limit = table.Column<double>(type: "float", nullable: false),
+                    Percent = table.Column<double>(type: "float", nullable: false),
+                    Averaging = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    MeasurementId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementStandards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeasurementStandards_Measurement_MeasurementId",
+                        column: x => x.MeasurementId,
+                        principalTable: "Measurement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasurementValues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    MeasurementId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeasurementValues_Measurement_MeasurementId",
+                        column: x => x.MeasurementId,
+                        principalTable: "Measurement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeasurementIndexes_MeasurementId",
+                table: "MeasurementIndexes",
+                column: "MeasurementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeasurementStandards_MeasurementId",
+                table: "MeasurementStandards",
+                column: "MeasurementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeasurementValues_MeasurementId",
+                table: "MeasurementValues",
+                column: "MeasurementId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MeasurementIndexes");
+
+            migrationBuilder.DropTable(
+                name: "MeasurementStandards");
+
+            migrationBuilder.DropTable(
+                name: "MeasurementValues");
+
+            migrationBuilder.DropTable(
+                name: "Measurement");
+        }
+    }
+}
