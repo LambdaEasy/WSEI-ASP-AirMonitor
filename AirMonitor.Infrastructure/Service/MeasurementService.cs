a8e41a3dee4d2451be60e1b1f86197880b3a34b0
cafetamine <arek.bystrowicz@gmail.com>
2021-02-13 10:31:38 +0100

    add base implementation of Measurement service

---
+++ AirMonitor.Infrastructure/Service/MeasurementService.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AirMonitor.Core.Measurement;
+using AirMonitor.Core.Measurement.Command;
+using AirMonitor.Domain.Measurement.Dto;
+using AirMonitor.Util.Flow;
+using Microsoft.Extensions.Logging;
+
+namespace AirMonitor.Infrastructure.Service
+{
+    public class MeasurementService : IMeasurementFacade
+    {
+        private readonly ILogger<MeasurementService> _logger;
+
+        private readonly IMeasurementRepository _repository;
+
+        public MeasurementService(ILogger<MeasurementService> logger, IMeasurementRepository repository)
+        {
+            this._logger = logger ?? throw new ArgumentException("Logger is null.");
+            this._repository = repository ?? throw new ArgumentException("IMeasurementRepository is null.");
+        }
+
+        // TODO based of installation id!!!
+        public Either<MeasurementError, MeasurementDto> Create(MeasurementCreateCommand command)
+        {
+            return TracedOperation.CallSync
+            (
+                _logger,
+                MeasurementOperationType.CreateMeasurement,
+                command,
+                () => _repository.TrySave(command.ToDomain())
+                                 .Map(MeasurementDto.FromDomain)
+                                 .ToEither(MeasurementError.DuplicateExternalId(command.ExternalId))
+            );
+        }
+
+        public Either<MeasurementError, MeasurementDto> GetById(long id)
+        {
+            return TracedOperation.CallSync
+            (
+                _logger,
+                MeasurementOperationType.GetIMeasurementById,
+                id,
+                () => _repository.FindById(id)
+                                 .Map(MeasurementDto.FromDomain)
+                                 .ToEither(MeasurementError.NotFoundById(id))
+            );
+        }
+
+        public Either<MeasurementError, MeasurementDto> GetByExternalId(long externalId)
+        {
+            return TracedOperation.CallSync
+            (
+                _logger,
+                MeasurementOperationType.GetIMeasurementById,
+                externalId,
+                () => _repository.FindByExternalId(externalId)
+                                 .Map(MeasurementDto.FromDomain)
+                                 .ToEither(MeasurementError.NotFoundByExternalId(externalId))
+            );
+        }
+
+        public ISet<MeasurementDto> GetAll()
+        {
+            return TracedOperation.CallSync
+            (
+                _logger,
+                MeasurementOperationType.GetAllMeasurements,
+                "none",
+                () => _repository.FindAll()
+                                 .Select(MeasurementDto.FromDomain)
+                                 .ToHashSet()
+            );
+        }
+
+        public Either<MeasurementError, MeasurementDto> Update(MeasurementUpdateCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(MeasurementDeleteCommand command)
+        {
+            return TracedOperation.CallSync
+            (
+                _logger,
+                MeasurementOperationType.DeleteMeasurement,
+                command,
+                () => _repository.DeleteById(command.Id)
+            );
+        }
+    }
+
+    public enum MeasurementOperationType
+    {
+        CreateMeasurement,
+        GetIMeasurementById,
+        GetAllMeasurements,
+        UpdateMeasurement,
+        DeleteMeasurement
+    }
+}
